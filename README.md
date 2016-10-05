2b51122e9b9cf54b262c654a6ca6c7b4ca9e8c22
Vitaliy Petrov <petrov@rlclub.pro>
2016-10-06 02:33:18 +0300

    'OpenGB','Close', 'NewBucket' fucn added. Not tested

--- README.md
+++ README.md
@@ -3,10 +3,9 @@ Simplified interface for work with BoldDB database
 
 Roadmap
 
- - [ ] Open database
- - [ ] Function to safe close database
- - [ ] Create new bucket
- - [ ] Create new bucket with key as id
+ - [ ] Open database "OpenGB"
+ - [ ] Function to safe close database "Close"
+ - [ ] Create new bucket "NewBucket"
  - [ ] Create new object in bucket
  - [ ] Update object in bucket
  - [ ] Delete object in bucket
@@ -19,6 +18,7 @@ Roadmap
  - [ ] Limit for objects list size
  - [ ] Sort list of objects by key
  - [ ] Sort list of objects by value
+ - [ ] Create new bucket with key as id
  - [ ] Make tests
  - [ ] Create documentation with examples
  - [ ] Something else
---
+++ gobolt.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	// "encoding/binary"
+	// "encoding/json"
+	// "fmt"
+	// "strconv"
+
+	"github.com/boltdb/bolt"
+)
+
+// Data base location settings
+type GoBoltConf struct {
+	path string
+}
+
+type Gobolt struct {
+	gb *bolt.DB
+}
+
+// type Object struct {
+//   ID   int    `json:"id"`
+//   Name string `json:"name"`
+//   Data string `json:"data"`
+// }
+
+// Open database
+func (gbase *Gobolt) OpenGB(gbc GoBoltConf) error {
+	var err error
+	// ToDo: in safe mode don't create database if it's not exist.
+	gbase.gb, err = bolt.Open(gbc.path, 0644, nil)
+
+	return err
+}
+
+// Safe close database.
+// Don't forget to use it after opening.
+// Ex.: defer database.Close()
+func (gbase *Gobolt) Close() {
+	gbase.gb.Close()
+}
+
+// Create new bucket
+func (gbase *Gobolt) NewBucket(bucket_name string) (*bolt.Bucket, error) {
+	bucket := new(bolt.Bucket)
+	err := gbase.gb.Update(func(tx *bolt.Tx) error {
+		var err error
+		bucket, err = tx.CreateBucketIfNotExists([]byte(bucket_name))
+		return err
+	})
+
+	return bucket, err
+}
+
+func main() {
+
+}
